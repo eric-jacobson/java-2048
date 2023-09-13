018b0566f1532e0ddb573283bbaab1012be8defa
eric-jacobson <ecjacobson9@gmail.com>
2023-09-12 21:01:55 -0600

    initial commit

---
+++ src/Game.java
@@ -0,0 +1,157 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+    private int[][] gameBoard = new int[4][4];
+    private int score = 0;
+
+    public void play() {
+        System.out.println("Begin game: ");
+        fillNewCell(gameBoard);
+        fillNewCell(gameBoard);
+        printBoard(gameBoard);
+
+        Scanner scanner = new Scanner(System.in);
+        while (!isGameOver(gameBoard)) {
+            String action = scanner.next();
+            System.out.println(action);
+
+            switch (action.toLowerCase()) {
+                case "w" -> {
+                    rotateBoardCounterClockwise(gameBoard);
+                    shiftCells(gameBoard);
+                    mergeCells(gameBoard);
+                    rotateBoardClockwise(gameBoard);
+                }
+                case "a" -> {
+                    shiftCells(gameBoard);
+                    mergeCells(gameBoard);
+                }
+                case "s" -> {
+                    rotateBoardClockwise(gameBoard);
+                    shiftCells(gameBoard);
+                    mergeCells(gameBoard);
+                    rotateBoardCounterClockwise(gameBoard);
+                }
+                case "d" -> {
+                    reverseBoard(gameBoard);
+                    shiftCells(gameBoard);
+                    mergeCells(gameBoard);
+                    reverseBoard(gameBoard);
+                }
+                default -> System.out.println("Unsupported action: " + action);
+            }
+
+            fillNewCell(gameBoard);
+            printBoard(gameBoard);
+        }
+
+    }
+
+    private boolean isGameOver(int[][] gameBoard) {
+        if (score == 2048) {
+            System.out.println("You win!!!");
+            return true;
+        }
+
+        //TODO: determine if game has reached a fail state, no possible moves
+
+        return false;
+    }
+
+    private void fillNewCell(int[][] gameBoard) {
+        List<String> emptyCells = new ArrayList<>();
+        // add all cells with 0 value to list
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard[i].length; j++) {
+                if (gameBoard[i][j] == 0) {
+                    emptyCells.add(i + "," + j);
+                }
+            }
+        }
+
+        // select random cell from list and add 2, enhancement add a chance to add a 4
+        Random random = new Random();
+        String[] indexes = emptyCells.get(random.nextInt(emptyCells.size())).split(",");
+        gameBoard[Integer.parseInt(indexes[0])][Integer.parseInt(indexes[1])] = 2;
+    }
+
+    private void shiftCells(int[][] gameBoard) {
+        int[][] newGameBoard = new int[4][4];
+
+        for (int i = 0; i < gameBoard.length; i++) {
+            int nextIndex = 0;
+            for (int j = 0; j < gameBoard[i].length; j++) {
+                if (gameBoard[i][j] != 0) {
+                    newGameBoard[i][nextIndex] = gameBoard[i][j];
+                    nextIndex++;
+                }
+            }
+        }
+
+        this.gameBoard = newGameBoard;
+    }
+
+    private void mergeCells(int[][] gameBoard) {
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard.length - 1; j++) {
+                if (gameBoard[i][j] == gameBoard[i][j + 1]) {
+                    gameBoard[i][j] = gameBoard[i][j] + gameBoard[i][j + 1];
+                    gameBoard[i][j + 1] = 0;
+                    score += gameBoard[i][j];
+                }
+                shiftCells(gameBoard);
+            }
+        }
+    }
+
+    private void reverseBoard(int[][] gameBoard) {
+        for (int[] row : gameBoard) {
+            int i = 0;
+            int j = row.length - 1;
+            while (i < j) {
+                int temp = row[i];
+                row[i] = row[j];
+                row[j] = temp;
+                i++;
+                j--;
+            }
+        }
+    }
+
+    private void rotateBoardClockwise(int[][] gameBoard) {
+        int X = gameBoard.length;
+        int Y = gameBoard[0].length;
+        int[][] newGameBoard = new int[X][Y];
+        for (int i = 0; i < X; i++) {
+            for (int j = 0; j < Y; j++) {
+                newGameBoard[j][X - 1 - i] = gameBoard[i][j];
+            }
+        }
+
+        this.gameBoard = newGameBoard;
+    }
+
+    private void rotateBoardCounterClockwise(int[][] gameBoard) {
+        int X = gameBoard.length;
+        int Y = gameBoard[0].length;
+        int[][] newGameBoard = new int[X][Y];
+        for (int i = 0; i < X; i++) {
+            for (int j = 0; j < Y; j++) {
+                newGameBoard[X - 1 - j][i] = gameBoard[i][j];
+            }
+        }
+
+        this.gameBoard = newGameBoard;
+    }
+
+    private void printBoard(int[][] gameBoard) {
+        System.out.println("score: " + this.score);
+        for (int[] row : gameBoard) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+}
